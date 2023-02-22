0b04baa058e52d088f063f82dddbe540b0b864b5
abhishekson <pandeyshailesh2081998@gmail.com>
2023-02-22 20:05:30 +0530

    -Revision of variables and add some off the  notes here ...

--- 002Basics/01variables.js
+++ 002Basics/01variables.js
@@ -20,3 +20,32 @@ var loggedCount = 34;
 var paymentMode;
 console.log(paymentMode);
 paymentMode = "Credit card";
+ 
+/*-------------------------------Revision of variables------------------------------------------------- */
+
+const accountId = 123321
+let accountEmail = "shailesh@123.com"
+var accountPassword = "123456"
+accountCity = "Gondia"
+let accountState;
+
+// accountId =4
+// console.log(accountId);
+
+/*
+NOTE: const value can not be change in the prog.
+        If you not initilized the value of variable  then it should consider as undefined .....
+ */
+
+accountEmail="shailu@1.com"
+accountPassword ="908999"
+accountCity ="nagpur"
+console.table([accountEmail,accountId,accountPassword,accountState,accountCity])
+
+
+
+/* 
+NOTE: Prefer not to use var
+        Because of issue in block scope and functional scope 
+        and use of semicolun (;) is not compulsory in javascript..... 
+ */
