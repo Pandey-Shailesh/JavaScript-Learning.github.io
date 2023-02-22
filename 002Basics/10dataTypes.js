9c696944cc4d3a8888a18d5b424646e7fd3cb505
abhishekson <pandeyshailesh2081998@gmail.com>
2023-02-22 21:59:10 +0530

    learn about conversion of data type

---
+++ 002Basics/10dataTypes.js
@@ -0,0 +1,34 @@
+"use strict"; //treat all JS code as newer version..
+
+//alert(3+4) //we are using node js not browser
+
+console.log(3 + 7);
+console.log("shailesh");
+/*
+NOTE: code readability should be high ....
+        documention : Read from mdn and ecmaScript(tc39.es)
+        ecmaScript(tc39.es)---> it is used to read about standarded (Specification) of javaScript
+*/
+ 
+let name ="shailu"
+let age =18
+let isLoggedin =true
+
+/**
+ * //premative
+ number
+ bigint
+ string  => "" or ''
+ boolean => true or false
+ null => standalone value  (object)
+ undefined =>
+ symbol => (Impliementation we see  when we read RECT) unique
+
+ //object
+
+ */
+
+ console.log(typeof "shailu")
+ console.log(typeof age)
+ console.log(typeof null);  //object 
+ console.log(typeof undefined);//undefined
---
+++ 002Basics/11conversionOperation.js
@@ -0,0 +1,35 @@
+let score = "shailu"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+/*
+NOTE: we use conversion operation to conver one type of data type into another type 
+ */
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+//null => 0
+//undefined =>NaN
+
+let isLoggedIn = "shailu"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+/console.log(stringNumber);
+ console.log(typeof stringNumber);
+
