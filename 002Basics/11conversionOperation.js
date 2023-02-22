08170ac3d3ec7c2a2dc0803c3508a13a49b50404
abhishekson <pandeyshailesh2081998@gmail.com>
2023-02-22 22:18:04 +0530

    learn about operations

--- 002Basics/11conversionOperation.js
+++ 002Basics/11conversionOperation.js
@@ -33,3 +33,44 @@ let stringNumber = String(someNumber)
 /console.log(stringNumber);
  console.log(typeof stringNumber);
 
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(5*2);
+console.log(2**5);
+console.log(2%3);
+console.log(2/3);
+
+let str1 = "hello "
+let str2 = "shailu"
+
+let str3 = str1 + str2
+ console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");    //it is also type of conversion
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 5 + 2
+
+let gameCounter = 100
+++gameCounter;  //postfix operator
+gameCounter++;  //prefix operator
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+//mdn 
